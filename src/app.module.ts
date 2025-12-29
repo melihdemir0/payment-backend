22c5285041163bd7e2316605f7097e9c16946306
Melih Demir <melihdemirr0@gmail.com>
2025-12-29 16:36:57 +0300

    feat(payment): initialize payment module (#5)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmRootModule } from './database/typeorm.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmRootModule, UsersModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmRootModule, UsersModule, AuthModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [UsersModule, JwtModule.register({})],
+  controllers: [AuthController],
   providers: [AuthService],
-  controllers: [AuthController]
 })
 export class AuthModule {}
