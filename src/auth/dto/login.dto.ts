405ba8986f970c8d5b633bbdeb22730a5040811b
Melih Demir <melihdemirr0@gmail.com>
2025-12-30 12:47:09 +0300

    swagger init (#6)

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,12 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty({ example: 'test@test.com' })
   @IsEmail()
-  email!: string;
+  email: string;
 
+  @ApiProperty({ example: 'test123' })
   @IsString()
-  @MinLength(6)
-  password!: string;
+  password: string;
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty({ example: 'test@test.com' })
   @IsEmail()
-  email!: string;
+  email: string;
 
+  @ApiProperty({ example: 'test' })
   @IsOptional()
   @IsString()
   username?: string;
 
+  @ApiProperty({ example: 'test123', minLength: 6 })
   @IsString()
   @MinLength(6)
-  password!: string;
+  password: string;
 }
